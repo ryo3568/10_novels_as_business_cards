dcc2219d8231b1e8086b0a4ed38d8eec8e988358
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-25 23:19:21 +0900

    [add] ランディングページの修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import '@/styles/globals.css'
-import Header from "../components/header"
-import Footer from "../components/footer"
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Header/>
-      <Component {...pageProps} />
-      <Footer/>
-    </div>
-  )
+  return <Component {...pageProps} />
 }
