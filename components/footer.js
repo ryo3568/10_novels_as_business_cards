f28f7a469efd8d80edfd17e96863fb34b8fb0757
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-25 23:44:20 +0900

    [add] footerの修正#5

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
+import Image from "next/image"
 
 const Footer = () => {
     return (
         <footer>
+            <Image src="/footer-logo.png" width="300" height="100" alt="tenpensya-logo"/>
             <p>©{new Date().getFullYear()} Tenpensya</p>
         </footer>
     )
