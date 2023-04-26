c1ca7815a22462e74d30067606d8f1addfba3217
ryowhale <kkujira619@gmail.com>
2023-04-26 23:25:48 +0900

    tailwindの確認#11

--- pages/index.js
+++ pages/index.js
@@ -19,35 +19,12 @@ const LandingPage = () => {
       </header>
       <h1>ランディングページ</h1>
       {/* 以下はTailWindCSSの適用例です。消してもかまいません */}
-      <div className="min-h-screen flex-col flex justify-center items-center">
-      <h1 className="text-4xl mb-5">Table</h1>
-      <table className="border-collapse border border-slate-400">
-        <thead>
-          <tr>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">果物</th>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">野菜</th>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">肉</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">りんご</td>
-            <td className="border border-slate-500 px-4 py-2">きゅうり</td>
-            <td className="border border-slate-500 px-4 py-2">鶏肉</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">ぶどう</td>
-            <td className="border border-slate-500 px-4 py-2">なす</td>
-            <td className="border border-slate-500 px-4 py-2">豚肉</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">みかん</td>
-            <td className="border border-slate-500 px-4 py-2">とまと</td>
-            <td className="border border-slate-500 px-4 py-2">牛肉</td>
-          </tr>
-        </tbody>
-       </table>
-      </div>
+      <Image src="/img1.jpg" width="482" height="150"/>
+      <h3><span>STEP1</span>アカウントの作成</h3>
+      <h3><span>STEP2</span>名刺の基本情報登録</h3>
+      <h3><span>STEP3</span>名刺がわりの小説10選登録</h3>
+      <h3><span>STEP4</span>コメントを追加</h3>
+      <h3><span>STEP5</span>QRコードのダウンロード</h3>
     </div>
   )
 }
