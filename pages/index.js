a3303b83cd6660f9876ed5c3a42e75ea83c5049e
ryowhale <kkujira619@gmail.com>
2023-04-27 18:19:52 +0900

    #25ランディングページのcss追加

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <div>
       <Head><title>名刺がわりの小説10選</title></Head>
       <header>
-          <div><Link href="/">
+          <div className="header-logo"><Link href="/">
             <Image src="/header-logo.png" width="482" height="150" alt="header logo"/>
           </Link></div>
           <nav>
@@ -17,37 +17,16 @@ const LandingPage = () => {
               </ul>
           </nav>
       </header>
+
       <h1>ランディングページ</h1>
       {/* 以下はTailWindCSSの適用例です。消してもかまいません */}
-      <div className="min-h-screen flex-col flex justify-center items-center">
-      <h1 className="text-4xl mb-5">Table</h1>
-      <table className="border-collapse border border-slate-400">
-        <thead>
-          <tr>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">果物</th>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">野菜</th>
-            <th className="border border-slate-500 px-4 py-2 bg-slate-100 font-bold">肉</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">りんご</td>
-            <td className="border border-slate-500 px-4 py-2">きゅうり</td>
-            <td className="border border-slate-500 px-4 py-2">鶏肉</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">ぶどう</td>
-            <td className="border border-slate-500 px-4 py-2">なす</td>
-            <td className="border border-slate-500 px-4 py-2">豚肉</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-500 px-4 py-2">みかん</td>
-            <td className="border border-slate-500 px-4 py-2">とまと</td>
-            <td className="border border-slate-500 px-4 py-2">牛肉</td>
-          </tr>
-        </tbody>
-       </table>
-      </div>
+
+      <Image src="/img1.jpg" width="1000" height="500" alt="landing-photo"/>
+      <h3><span>01</span>アカウントを作成しよう</h3>
+      <h3><span>02</span>名刺の基本情報を入力しよう</h3>
+      <h3><span>03</span>名刺がわりにしたい本を10冊登録しよう</h3>
+      <h3><span>04</span>本についてのコメントを登録しよう</h3>
+      <h3><span>05</span>QRコードをダウンロードしよう</h3>
     </div>
   )
 }
