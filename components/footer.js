9aad6ca7941acb76f9479b268f4ca53ee2e3bcca
ryowhale <kkujira619@gmail.com>
2023-04-28 20:58:18 +0900

    #50 フッターcss調整

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,8 @@ import Image from "next/image"
 const Footer = () => {
     return (
         <footer>
-            <Image src="/footer-logo.png" width="300" height="100" alt="tenpensya-logo"/>
-            <p>©{new Date().getFullYear()} Tenpensya</p>
+            <Image className="footer-logo" src="/footer-logo.png" width="300" height="100" alt="tenpensya-logo"/>
+            <p className="copy">©{new Date().getFullYear()} Tenpensya</p>
         </footer>
     )
 }
